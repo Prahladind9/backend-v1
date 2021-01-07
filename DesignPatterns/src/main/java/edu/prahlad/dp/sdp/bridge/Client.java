11e07e1d775b0514cf9639b46358333b649284d2
Prahlad Rao <prahladskingdom@hotmail.com>
2021-01-07 19:33:42 +0530

    Bridge - SDP

---
+++ DesignPatterns/src/main/java/edu/prahlad/dp/sdp/bridge/Client.java
@@ -0,0 +1,16 @@
+package edu.prahlad.dp.sdp.bridge;
+
+public class Client {
+    public static void main(String[] args) {
+        FifoCollection<Integer> collection = new Queue<>(new SinglyLinkedList<>());
+        collection.offer(10);
+        collection.offer(40);
+        collection.offer(80);
+
+        System.out.println(collection.poll());
+        System.out.println(collection.poll());
+        System.out.println(collection.poll());
+
+        System.out.println(collection.poll());
+    }
+}
