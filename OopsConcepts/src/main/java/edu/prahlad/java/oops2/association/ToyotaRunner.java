cb8870eee1e63340b748d202a1c1c614b3968d34
Prahlad Rao <prahladskingdom@hotmail.com>
2021-01-11 16:29:53 +0530

    Oops - Examples

---
+++ OopsConcepts/src/main/java/edu/prahlad/java/oops2/association/ToyotaRunner.java
@@ -0,0 +1,67 @@
+package edu.prahlad.java.oops2.association;
+
+// Car class
+class CarG {
+
+    // Declaring data members and methods
+    private int id;
+    private String model;
+    private String color;
+
+    public void carFeatures() { // Function to print out car features
+        System.out.println("Car Model: " + model);
+        System.out.println("Car Color: " + color);
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
+
+// Toyota Class, which is a child class of Car class.
+class Toyota extends CarG {
+
+    // Inherits all properties of Car class
+    public void setStart() {
+        // Declaring an engine object and calling the start() function of the engine.
+        ToyotaEngine engine = new ToyotaEngine();
+        engine.start();
+    }
+
+}
+
+// Engine class
+class ToyotaEngine {
+
+    // This function simply prints out on screen that Engine has been started!
+    public void start() {
+        System.out.println("Engine has been started.");
+    }
+
+    // This function simply prints out on screen that Engine has been stopped!
+    public void stop() {
+        System.out.println("Engine has been stopped.");
+    }
+
+}
+
+/**
+ * You already know that in such a composition relation,
+ * the Toyota class will be responsible for Engine’s lifetime.
+ */
+public class ToyotaRunner {
+    public static void main(String[] args) {
+        // Declaring and initializing Toyota object
+        Toyota t = new Toyota();
+        t.setModel("Fortuner");
+        t.setColor("Silver");
+
+        t.carFeatures();
+        t.setStart();
+    }
+}
---
+++ OopsConcepts/src/main/java/edu/prahlad/java/oops2/association/VehicleRunner.java
@@ -0,0 +1,42 @@
+package edu.prahlad.java.oops2.association;
+
+class Vehicle {
+
+    // Complete this implementation
+    int id;
+    String model;
+
+    public Vehicle(String model, int id){
+        this.id = id;
+        this.model = model;
+    }
+
+}
+
+class Driver {
+
+    // Complete this implementation
+    String driverName;
+    Vehicle vehicle;
+
+    public Driver(String driverName, Vehicle vehicle){
+        this.driverName = driverName;
+        this.vehicle = vehicle;
+    }
+
+}
+
+//Association example
+public class VehicleRunner {
+
+    public static void main(String args[]) {
+        // Creating a Vehicle object with model: "Volvo S60", and id: "4453"
+        Vehicle vehicle = new Vehicle("Volvo S60", 4453);
+        // Creating a Driver object having name: "John" and passing the
+        // vehicle in its constructor
+        Driver driver = new Driver("John", vehicle);
+        System.out.println(driver.driverName +
+                " is a driver of car Id: " + driver.vehicle.id);
+    }
+
+}
