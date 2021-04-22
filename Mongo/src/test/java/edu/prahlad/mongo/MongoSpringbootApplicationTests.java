3818c1a9db74eef9607f63d4eea5a17936adee8f
Prahlad Rao <prahladskingdom@hotmail.com>
2021-04-22 23:37:42 +0530

    Mongo - Initial Setup

---
+++ Mongo/src/test/java/edu/prahlad/mongo/MongoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.prahlad.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
