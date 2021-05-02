ce2376a85cbbf16f63ae25647b5e52035de73d1b
Prahlad Rao <prahladskingdom@hotmail.com>
2021-05-02 19:30:08 +0530

    Room Service Endpoint Setup

---
+++ Spring/spring-cloud/room-services/src/main/java/edu/prahlad/lil/roomservices/RoomRepository.java
@@ -0,0 +1,8 @@
+package edu.prahlad.lil.roomservices;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long> {
+}
