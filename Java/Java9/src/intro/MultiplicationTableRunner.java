986c8bd309b1fb5f7128f159ba2c610412c86e53
Prahlad Rao <prahladskingdom@hotmail.com>
2021-05-05 18:25:37 +0530

    Intro Java

---
+++ Java/Java9/src/intro/MultiplicationTableRunner.java
@@ -0,0 +1,11 @@
+package intro;
+
+public class MultiplicationTableRunner {
+    public static void main(String[] args) {
+//        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        MultiplicationTableRefactored multiplicationTable = new MultiplicationTableRefactored();
+        multiplicationTable.print();
+        multiplicationTable.print(9);
+        multiplicationTable.print(16,11,20);
+    }
+}
