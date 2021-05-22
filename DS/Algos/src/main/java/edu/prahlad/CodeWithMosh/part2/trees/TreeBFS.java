1d2881aa72bfcfd946dadfd77a209c838d8fbcd4
Prahlad Rao <prahladskingdom@hotmail.com>
2021-05-22 17:50:45 +0530

    DS - BinaryTree

---
+++ DS/Algos/src/main/java/edu/prahlad/CodeWithMosh/part2/trees/TreeBFS.java
@@ -0,0 +1,17 @@
+package edu.prahlad.CodeWithMosh.part2.trees;
+
+public class TreeBFS extends Tree{
+    public void traverseLevelOrder(){
+        for (int i = 0; i <= height(); i++) {
+            for (var value: getNodesAtDistance(i))
+                System.out.println(value);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeBFS tree = new TreeBFS();
+        tree.getInitializedTree(tree);
+        System.out.println(tree);
+        tree.traverseLevelOrder();
+    }
+}
---
+++ DS/Algos/src/main/java/edu/prahlad/CodeWithMosh/part2/trees/TreeHelper.java
@@ -0,0 +1,16 @@
+package edu.prahlad.CodeWithMosh.part2.trees;
+
+@FunctionalInterface
+public interface TreeHelper {
+
+    void customInitializeTree(final Tree tree);
+    default void getInitializedTree(final Tree tree){
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+    }
+}
