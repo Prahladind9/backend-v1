f1942e7cecd13de49c4335349d272c6926795799
Prahlad Rao <prahladskingdom@hotmail.com>
2021-05-31 23:04:26 +0530

    Insertion Sort

--- DS/Algos/src/main/java/edu/prahlad/CodeWithMosh/part3/sorting/ZSortRunner.java
+++ DS/Algos/src/main/java/edu/prahlad/CodeWithMosh/part3/sorting/ZSortRunner.java
@@ -6,7 +6,8 @@ public class ZSortRunner {
     public static void main(String[] args) {
         int[] numbers = {83, 1, 3, 5, 6};
 //        var sorter = new BubbleSort();
-        var sorter = new SelectionSort();
+//        var sorter = new SelectionSort();
+        var sorter = new InsertionSort();
         sorter.sort(numbers);
         System.out.println(Arrays.toString(numbers));
     }
