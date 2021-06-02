00368ae91b45b10264ddfcebf81efc7e52496c72
Prahlad Rao <prahladskingdom@hotmail.com>
2021-06-02 18:45:19 +0530

    Bucket Sort

--- DS/Algos/src/main/java/edu/prahlad/CodeWithMosh/part3/sorting/ZSortRunner.java
+++ DS/Algos/src/main/java/edu/prahlad/CodeWithMosh/part3/sorting/ZSortRunner.java
@@ -4,11 +4,18 @@ import java.util.Arrays;
 
 public class ZSortRunner {
     public static void main(String[] args) {
-        int[] numbers = {83, 1, 3, 5, 6};
+        int[] numbers = {8, 1, 3, 5, 8, 6, 19};
 //        var sorter = new BubbleSort();
 //        var sorter = new SelectionSort();
-        var sorter = new InsertionSort();
-        sorter.sort(numbers);
+//        var sorter = new InsertionSort();
+//        var sorter = new MergeSort();
+//        var sorter = new QuickSort();
+//        sorter.sort(numbers);
+
+//        var sorter = new CountingSort();
+//        sorter.sort(numbers, 20);
+        var sorter = new BucketSort();
+        sorter.sort(numbers, 6);
         System.out.println(Arrays.toString(numbers));
     }
 }
