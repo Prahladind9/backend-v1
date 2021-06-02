0bb6bd25191f6cda7d70a579caa26236c3cde4bb
Prahlad Rao <prahladskingdom@hotmail.com>
2021-06-02 17:23:06 +0530

    Quick Sort

---
+++ DS/Algos/src/main/java/edu/prahlad/CodeWithMosh/part3/sorting/QuickSort.java
@@ -0,0 +1,39 @@
+package edu.prahlad.CodeWithMosh.part3.sorting;
+
+/**
+ *              Best        Worst
+ * Partitioning O(n)        O(n)
+ * # of items   O(log n)    O(n)
+ * Total        O(n log n)  O(n^2)
+ * <p>
+ * Space        O(log n)     O(n)
+ */
+public class QuickSort implements Sort {
+    @Override
+    public void sort(int[] array) {
+        sort(array, 0, array.length - 1);
+    }
+
+    private void sort(int[] array, int start, int end) {
+        //base case
+        if (start >= end) return;
+
+        //partition
+        var boundary = partition(array, start, end);
+        //sort left
+        sort(array, start, boundary - 1);
+        //sort right
+        sort(array, boundary + 1, end);
+    }
+
+    private int partition(int[] array, int start, int end) {
+        var pivot = array[end];
+        var boundary = start - 1;
+        for (int i = start; i <= end; i++) {
+            if (array[i] <= pivot)
+                swap(array, i, ++boundary);
+        }
+
+        return boundary;
+    }
+}
