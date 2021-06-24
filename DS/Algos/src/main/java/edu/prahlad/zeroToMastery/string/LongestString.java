a1ea91e74f65ff51b8326924aad0cacfddb926e5
Prahlad Rao <prahladskingdom@hotmail.com>
2021-06-24 23:35:47 +0530

    String - LongestSubstring - 1

---
+++ DS/Algos/src/main/java/edu/prahlad/zeroToMastery/string/LongestString.java
@@ -0,0 +1,29 @@
+package edu.prahlad.zeroToMastery.string;
+
+/**
+ * Given a string, find the length of the longest
+ * substring without repeating characters
+ */
+public class LongestString {
+
+    /**
+     * 0) Visualize the question
+     *     "abccabb" - 3
+     *
+     * 1) Verify the constraints
+     *      a) Is the substring contiguous?
+     *          Yes, look for a substring not a subsequence
+     *              substring vs subsequence
+     *      b) Does case sensitivity matter?
+     *          No, assume all characters in the string are lowercase
+     *
+     * 2) Write out some test cases
+     *      "abccabb"   3
+     *      "cccccc"    1
+     *      ""          0
+     *      "abcbda"    4
+     *
+     * 3)      
+     *
+     */
+}
