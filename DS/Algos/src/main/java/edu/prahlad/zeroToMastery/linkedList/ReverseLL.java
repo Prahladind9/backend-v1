0488a4d62f12a68c1c48feebfa7cfdcae3cdbba8
Prahlad Rao <prahladskingdom@hotmail.com>
2021-07-05 23:05:42 +0530

    LinkedList - 1

---
+++ DS/Algos/src/main/java/edu/prahlad/zeroToMastery/linkedList/ReverseLL.java
@@ -0,0 +1,23 @@
+package edu.prahlad.zeroToMastery.linkedList;
+
+/**
+ * Given a linked list, return it in reverse
+ */
+public class ReverseLL {
+
+    /**
+     * 0) Visualize the problem
+     *
+     * 1) Verify the constraints
+     *      What do we return if we get null or single node?
+     *      Just return null and the node back respectively/
+     *
+     * 2) Write out some test cases
+     *      a) 1 > 2 > 3 > 4 > 5 > null; soln: 5 > 4 > 3 > 2 > 1 > null
+     *      b) 3    ; soln: 3
+     *      b) null ; soln: null
+     *
+     *  3) Figure out a solution without code
+     *
+     */
+}
