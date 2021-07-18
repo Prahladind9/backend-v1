95ea4695a809e7e175450070453b4376f2bd7756
Prahlad Rao <prahladskingdom@hotmail.com>
2021-07-18 18:13:25 +0530

    String - Reverse, RemoveString

---
+++ DS/Algos/src/main/java/edu/prahlad/educativeio/recursion/string/RemoveDuplicateString.java
@@ -0,0 +1,30 @@
+package edu.prahlad.educativeio.recursion.string;
+
+public class RemoveDuplicateString {
+
+    private static String remDuplicates(String text){
+        //base case
+        if(text.length() == 1){
+            return text;
+        }
+
+        //recursive case
+        if(text.substring(0,1).equals(text.substring(1,2))){
+            return remDuplicates(text.substring(1));
+        }else{
+            return text.substring(0,1) + remDuplicates(text.substring(1));
+        }
+
+    }
+
+    public static void main( String args[] ) {
+//        String input1 = "Helloo";
+        String input1 = "Thiss iiss aa teesstt";
+
+        System.out.println( "Original string: " + input1);
+
+        String output = remDuplicates(input1);
+
+        System.out.println("String after: " + output);
+    }
+}
