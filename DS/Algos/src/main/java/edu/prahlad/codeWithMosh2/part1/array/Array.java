2912a3a2fc138c0c29bb93e7a4ee57747f8273dd
Prahlad Rao <prahladskingdom@hotmail.com>
2021-08-05 23:22:12 +0530

    Insert & removeAt

--- DS/Algos/src/main/java/edu/prahlad/codeWithMosh2/part1/array/Array.java
+++ DS/Algos/src/main/java/edu/prahlad/codeWithMosh2/part1/array/Array.java
@@ -1,28 +1,48 @@
 package edu.prahlad.codeWithMosh2.part1.array;
 
 public class Array {
-    private int[] array;
+    private int[] items;
     private int count;
 
     public Array(int length) {
-        this.array = new int[length];
+        this.items = new int[length];
     }
 
-    public void insert(int item){
-        if(array.length == count){
-            int[] newArray = new int[count*2];
+    public void insert(int item) {
+        if (items.length == count) {
+            int[] newArray = new int[count * 2];
             for (int i = 0; i < count; i++) {
-                newArray[i] = array[i];
+                newArray[i] = items[i];
             }
-            array = newArray;
+            items = newArray;
         }
 
-        array[count++] = item;
+        items[count++] = item;
     }
 
-    public void print(){
+    public void removeAt(int index) {
+        if (index < 0 || index >= count)
+            throw new IllegalArgumentException();
+
+        for (int i = index; i < count - 1; i++) {
+            items[i] = items[i + 1];
+        }
+
+        count--;
+    }
+
+    public int indexOf(int item){
         for (int i = 0; i < count; i++) {
-            System.out.println(array[i]);
+            if(items[i] == item)
+                return i;
+        }
+
+        return -1;
+    }
+
+    public void print() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(items[i]);
         }
     }
 
@@ -31,6 +51,9 @@ public class Array {
         array.insert(10);
         array.insert(20);
         array.insert(30);
+        array.insert(40);
+        array.removeAt(3);
+        System.out.println(":)  " + array.indexOf(30));
         array.print();
     }
 }
