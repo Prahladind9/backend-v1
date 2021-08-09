8075149da621e201d76dc45b6bc1f6f868853406
Prahlad Rao <prahladskingdom@hotmail.com>
2021-08-09 22:02:52 +0530

    LinkedList - 2

--- DS/Algos/src/main/java/edu/prahlad/codeWithMosh2/part1/linkedList/LinkedList.java
+++ DS/Algos/src/main/java/edu/prahlad/codeWithMosh2/part1/linkedList/LinkedList.java
@@ -1,4 +1,22 @@
 package edu.prahlad.codeWithMosh2.part1.linkedList;
 
 public class LinkedList {
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+            this.next = null;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", next=" + next +
+                    '}';
+        }
+    }
 }
