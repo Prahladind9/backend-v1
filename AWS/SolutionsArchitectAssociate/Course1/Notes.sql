0408336269e9ba91b09b86184b18a4ec28b0937b
Prahlad Rao <prahladskingdom@hotmail.com>
2021-08-23 23:15:56 +0530

    AWS - 04

---
+++ AWS/SolutionsArchitectAssociate/Course1/Notes.sql
@@ -0,0 +1,13 @@
+(1) Getting started with AWS Virtual Servers - EC2 Service and EC2 Instances
+General Commands
+    a)  whoami
+    b)  python --version
+    c)  ls
+    d)  pwd
+    e)  sudo su
+    f)  yum install httpd -y
+    g)  service httpd status
+    h)  history
+
+Instance Metadata Service and Dynamic Data
+   curl http://169.254.169.254/latest/meta-data
