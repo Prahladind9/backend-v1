c2d129629b873e3ef246a21abc4d7a10921b94b9
Prahlad Rao <prahladskingdom@hotmail.com>
2021-08-26 16:54:55 +0530

    Linked List - Usage

--- DS/Algos/src/main/java/edu/prahlad/codeWithMosh2/part1/linkedList/LinkedList.java
+++ DS/Algos/src/main/java/edu/prahlad/codeWithMosh2/part1/linkedList/LinkedList.java
@@ -5,18 +5,18 @@ import java.util.NoSuchElementException;
 
 /**
  * Lookup
- *      By Value O(n)
- *      By Index O(n)
- *
- *  Insert
- *      At the End O(1)
- *      At the Start O(1)
- *      In the Middle O(n)
- *
- *  Delete
- *      At the End O(n)
- *      At the Start O(1)
- *      In the Middle O(n)
+ * By Value O(n)
+ * By Index O(n)
+ * <p>
+ * Insert
+ * At the End O(1)
+ * At the Start O(1)
+ * In the Middle O(n)
+ * <p>
+ * Delete
+ * At the End O(n)
+ * At the Start O(1)
+ * In the Middle O(n)
  */
 public class LinkedList {
 
@@ -31,10 +31,7 @@ public class LinkedList {
 
         @Override
         public String toString() {
-            return "Node{" +
-                    "value=" + value +
-                    ", next=" + next +
-                    '}';
+            return "Val: " + value + ", Next >" + next;
         }
     }
 
@@ -42,7 +39,7 @@ public class LinkedList {
     private Node last;
     private int size;
 
-    public int size(){
+    public int size() {
         return size;
     }
 
@@ -51,39 +48,41 @@ public class LinkedList {
     }
 
     //addFirst
-    public void addFirst(int value){
+    public void addFirst(int value) {
         var newNode = new Node(value);
-        if(isEmpty()){
+        if (isEmpty()) {
             this.first = newNode;
             this.last = this.first;
-        }else {
+        } else {
             newNode.next = first;
             first = newNode;
         }
 
         size++;
     }
+
     //addLast
-    public void addLast(int value){
+    public void addLast(int value) {
         Node newNode = new Node(value);
 
-        if(isEmpty()){
+        if (isEmpty()) {
             this.first = newNode;
             this.last = this.first;
-        }else {
+        } else {
             this.last.next = newNode;
             this.last = newNode;
         }
 
         size++;
     }
-    //deleteFirst
-    public void deleteFirst(){
-        if(isEmpty()) throw new NoSuchElementException();
 
-        if(first == last){
+    //deleteFirst
+    public void deleteFirst() {
+        if (isEmpty()) throw new NoSuchElementException();
+
+        if (first == last) {
             first = last = null;
-        }else {
+        } else {
 
             Node second = this.first.next;
             this.first.next = null;
@@ -92,14 +91,15 @@ public class LinkedList {
 
         size--;
     }
+
     //deleteLast
-    public void deleteLast(){
+    public void deleteLast() {
         if (isEmpty()) throw new NoSuchElementException();
 
-        if(first == last){
+        if (first == last) {
             first = last = null;
 
-        }else {
+        } else {
 //        Node newLastNode = getNodeAtIndex(size()-1); or
             Node newLastNode = getPrevious(this.last);
             newLastNode.next = null;
@@ -107,31 +107,32 @@ public class LinkedList {
         }
         size--;
     }
+
     //contains
-    public boolean contains(int value){
-       return indexOf(value) != -1;
+    public boolean contains(int value) {
+        return indexOf(value) != -1;
     }
 
     //indexOf
-    public int indexOf(int value){
+    public int indexOf(int value) {
         Node temp = this.first;
         for (int i = 1; i <= size; i++) {
-            if(temp.value == value) return i;
+            if (temp.value == value) return i;
             temp = temp.next;
         }
         return -1;
     }
 
-    private Node getPrevious(Node node){
+    private Node getPrevious(Node node) {
         var current = first;
-        while (current !=null){
-            if(current.next == node) return current;
+        while (current != null) {
+            if (current.next == node) return current;
             current = current.next;
         }
         return null;
     }
 
-    private Node getNodeAtIndex(int index){
+    private Node getNodeAtIndex(int index) {
         Node temp = this.first;
         for (int i = 1; i < index; i++) {
             temp = temp.next;
@@ -139,7 +140,7 @@ public class LinkedList {
         return temp;
     }
 
-    public int[] toArray(){
+    public int[] toArray() {
         int[] array = new int[size()];
         Node current = first;
         for (int i = 0; i < size(); i++) {
@@ -150,17 +151,79 @@ public class LinkedList {
         return array;
     }
 
-    @Override
-    public String toString() {
-        return "LinkedList{" +
-                "first=" + first +
-                ", last=" + last +
-                ", size=" + size +
-                '}';
+    public void reverse() {
+        if (isEmpty()) return;
+
+        //previous p current c next n
+        //1st Iteration
+        // 10 -> 20 -> 30
+        // p     c     n
+        // n = c.next
+        // c.next = p
+
+        //2nd Iteration
+        // 10 <- 20 -> 30
+        //      p     c     n
+        // n = c.next
+        // c.next = p
+
+        var previous = first;
+        var current = first.next;
+        while (current != null) {
+            var next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        last = first;
+        last.next = null;
+        first = previous;
+
     }
 
-    private void print(){
-        System.out.println(this);
+    public void reverseBF() {
+        if (isEmpty()) return;
+
+        Node current = first;
+        Node firstNode = first;
+        Node reversed = new Node(current.value);
+
+        while (current.next != null) {
+            current = current.next;
+
+            Node temp = reversed;
+            reversed = new Node(current.value);
+            reversed.next = temp;
+        }
+
+        this.last = firstNode;
+        this.last.next = null;
+        this.first = reversed;
+
+    }
+
+    public Node getKthNodeFromTheEnd(int k) {
+        if(k > size())
+            throw new IllegalArgumentException();
+
+        //2 pointer k-1 nodes apart
+        var a = first;
+        var b = first;
+        for (int i = 0; i < k - 1; i++) {
+            b = b.next;
+        }
+
+        while (b != last) {
+            a = a.next;
+            b = b.next;
+        }
+
+        return a;
+    }
+
+    private void print() {
+        System.out.println(Arrays.toString(this.toArray()));
     }
 
     public static void main(String[] args) {
@@ -171,6 +234,9 @@ public class LinkedList {
         linkedList.addFirst(5);
         linkedList.addLast(40);
         linkedList.print();
+
+        System.out.println("KthNodeFromTheEnd: "+linkedList.getKthNodeFromTheEnd(0));
+        System.out.println("KthNodeFromTheEnd: "+linkedList.getKthNodeFromTheEnd(5));
 
         linkedList.deleteFirst();
         linkedList.print();
@@ -185,6 +251,10 @@ public class LinkedList {
         System.out.println(linkedList.indexOf(20));
         System.out.println(linkedList.indexOf(90));
 
-        System.out.println(Arrays.toString(linkedList.toArray()));
+        linkedList.print();
+
+        linkedList.reverse();
+        linkedList.print();
+
     }
 }
