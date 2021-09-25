1cee85bf7278579e209735b35a648cf23010387e
Prahlad Rao <prahladskingdom@hotmail.com>
2021-09-25 19:40:00 +0530

    AVL Tree - 2

--- DS/Algos/src/main/java/edu/prahlad/codeWithMosh2/part2/treesAVL/AVLTree.java
+++ DS/Algos/src/main/java/edu/prahlad/codeWithMosh2/part2/treesAVL/AVLTree.java
@@ -2,6 +2,7 @@ package edu.prahlad.codeWithMosh2.part2.treesAVL;
 
 public class AVLTree {
     private class AVLNode {
-        
+        private AVLNode leftChild;
+        private AVLNode rightChild;
     }
 }
