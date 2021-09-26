eec7d19c45f96316379b00fa951a5a9a19d4091f
Prahlad Rao <prahladskingdom@hotmail.com>
2021-09-26 23:39:17 +0530

    Tree - 2

--- DS/Algos/src/main/java/edu/prahlad/codeWithMosh2/part2/trees/Tree.java
+++ DS/Algos/src/main/java/edu/prahlad/codeWithMosh2/part2/trees/Tree.java
@@ -1,4 +1,83 @@
 package edu.prahlad.codeWithMosh2.part2.trees;
 
 public class Tree {
+
+    private class Node {
+        public int value;
+        public Node leftChild;
+        public Node rightChild;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node leftChild, Node rightChild) {
+            this.value = value;
+            this.leftChild = leftChild;
+            this.rightChild = rightChild;
+        }
+    }
+
+    public Node root;
+
+
+    public void insert(final int value) {
+        //Better handling > but this case > isn't always required
+        if (find(value))
+            throw new IllegalArgumentException("Value already exists in the Tree");
+
+        final var newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+
+        var current = root;
+
+        while (true) {
+            if (current.value < value) {
+                if (current.leftChild == null) {
+                    current.leftChild = newNode;
+                    break;
+                }
+                current = current.leftChild;
+            }
+
+
+            if (current.value > value) {
+                if (current.rightChild == null) {
+                    current.rightChild = newNode;
+                    break;
+                }
+                current = current.rightChild;
+            }
+        }
+
+    }
+
+    public boolean find(final int value) {
+        if(root == null){
+            throw new IllegalStateException();
+        }
+
+        var current = root;
+        while (current != null) {
+            if (current.value == value) {
+                return true;
+            } else if (current.value < value) {
+                current = current.leftChild;
+            } else {
+                current = current.rightChild;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        //root(Node)
+        //Node(value, leftChild, rightChild)
+        //insert(value)
+        //find(value):boolean
+    }
 }
