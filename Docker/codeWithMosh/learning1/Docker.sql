0f1c1a2f704d47f4df365a54f7813bc94acb8405
Prahlad Rao <prahladskingdom@hotmail.com>
2021-10-04 22:10:46 +0530

    Docker - 1

---
+++ Docker/codeWithMosh/learning1/Docker.sql
@@ -0,0 +1,16 @@
+Docker 
+A platform for consistently > building, running and shipping applications
+
+Reasons code might fail
+1) One or more files missing
+2) Software version mismatch
+3) Differnet config settings
+
+Container: An isolated environment for running an application
+VirtualMachine: An abstraction of a machine (physical hardware)
+
+A kernel manages applications and hardware resources
+
+
+Commands:
+docker version
