f1243d527e9f312e50c6ddb2463e393547f85133
Prahlad Rao <prahladskingdom@hotmail.com>
2021-10-12 22:09:15 +0530

    LL - 1

---
+++ DS/Algos/src/main/java/edu/prahlad/educativeio/patternforCodingQuestions/inPlaceReversalOfLL/ReverseLinkedList.java
@@ -0,0 +1,51 @@
+package edu.prahlad.educativeio.patternforCodingQuestions.inPlaceReversalOfLL;
+
+/**
+ * Given the head of a Singly LinkedList, reverse the LinkedList.
+ * Write a function to return the new head of the reversed LinkedList.
+ *
+ * TC: O(n)
+ * SC: O(1)
+ */
+public class ReverseLinkedList {
+
+    public static ListNode reverse(ListNode head) {
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next = null;
+
+        while (current !=null){
+            next = current.next;
+            current.next = previous;
+
+            previous = current;
+            current = next;
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(2);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(6);
+        head.next.next.next = new ListNode(8);
+        head.next.next.next.next = new ListNode(10);
+
+        ListNode result = ReverseLinkedList.reverse(head);
+        System.out.print("Nodes of the reversed LinkedList are: ");
+        while (result != null) {
+            System.out.print(result.value + " ");
+            result = result.next;
+        }
+    }
+}
+
+class ListNode {
+    int value = 0;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
