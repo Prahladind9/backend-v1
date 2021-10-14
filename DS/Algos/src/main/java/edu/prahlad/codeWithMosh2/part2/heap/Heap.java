a102043966cf71b651ca04fa6a1b850366a8491f
Prahlad Rao <prahladskingdom@hotmail.com>
2021-10-14 20:50:11 +0530

    Heap - Implementation

--- DS/Algos/src/main/java/edu/prahlad/codeWithMosh2/part2/heap/Heap.java
+++ DS/Algos/src/main/java/edu/prahlad/codeWithMosh2/part2/heap/Heap.java
@@ -1,4 +1,129 @@
 package edu.prahlad.codeWithMosh2.part2.heap;
 
+import java.util.Arrays;
+
 public class Heap {
+    private int[] items = new int[10];
+    private int size;
+
+    public void insert(int value) {
+        if (isFull())
+            throw new IllegalStateException();
+
+        items[size++] = value;
+        bubbleUp();
+    }
+
+    public int remove() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        var root = items[0];
+        items[0] = items[--size];
+        bubbleDown();
+
+        return root;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void bubbleDown() {
+        var index = 0;
+        while (index <= size && !isValidParent(index)) {
+            int largerChildIndex = largerChildIndex(index);
+            swap(index, largerChildIndex);
+            index = largerChildIndex;
+        }
+    }
+
+    private int largerChildIndex(int index) {
+
+        if (!hasLeftChild(index)) return index;
+        if (!hasRightChild(index)) return leftChildIndex(index);
+
+        var largerChildIndex =
+                (leftChild(index) > rightChild(index)) ?
+                        leftChildIndex(index) :
+                        rightChildIndex(index);
+        return largerChildIndex;
+    }
+
+    private boolean hasLeftChild(int index) {
+        return leftChildIndex(index) <= size;
+    }
+
+    private boolean hasRightChild(int index) {
+        return rightChildIndex(index) <= size;
+    }
+
+    private boolean isValidParent(int index) {
+        if (!hasLeftChild(index)) return true;
+
+        var isValid = items[index] >= leftChild(index);
+
+        if (hasRightChild(index))
+            isValid &= items[index] >= rightChild(index);
+
+        return isValid;
+    }
+
+    private int rightChild(int index) {
+        return items[rightChildIndex(index)];
+    }
+
+    private int leftChild(int index) {
+        return items[leftChildIndex(index)];
+    }
+
+    private int leftChildIndex(int index) {
+        return index * 2 + 1;
+    }
+
+    private int rightChildIndex(int index) {
+        return index * 2 + 2;
+    }
+
+    private void bubbleUp() {
+        var index = size - 1;
+        while (index > 0 && items[index] > items[parentIndex(index)]) {
+            swap(index, parentIndex(index));
+            index = parentIndex(index);
+        }
+    }
+
+    private void swap(int index, int parentIndex) {
+        var temp = items[index];
+        items[index] = items[parentIndex];
+        items[parentIndex] = temp;
+    }
+
+    private int parentIndex(int index) {
+        return (index - 1) / 2;
+    }
+
+    private boolean isFull() {
+        return size == items.length;
+    }
+
+    @Override
+    public String toString() {
+        return "Heap{" +
+                "items=" + Arrays.toString(items) +
+                ", size=" + size +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Heap heap = new Heap();
+        heap.insert(10);
+        heap.insert(5);
+        heap.insert(17);
+        heap.insert(4);
+        heap.insert(22);
+        System.out.println(heap);
+        System.out.println("Remove: "   +heap.remove());
+        System.out.println(heap);
+    }
 }
