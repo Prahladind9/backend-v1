adf6c1534ac5da85f828d542b9ef6eef55b6deec
Prahlad Rao <prahladskingdom@hotmail.com>
2021-11-07 01:36:02 +0530

    Spring - M1 - Q10 - 2

---
+++ Spring/spring-exam/course1/module01/q10/scope-web-boot/src/main/java/com/spring/professional/beans/SpringBean3.java
@@ -0,0 +1,9 @@
+package com.spring.professional.beans;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class SpringBean3 {
+}
---
+++ Spring/spring-exam/course1/module01/q10/scope-web-boot/src/main/java/com/spring/professional/beans/SpringBean4.java
@@ -0,0 +1,9 @@
+package com.spring.professional.beans;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class SpringBean4 {
+}
