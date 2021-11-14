fe56d3ed93a3818ddca29615adced94b9e50c4ae
Prahlad Rao <prahladskingdom@hotmail.com>
2021-11-14 11:19:16 +0530

    Spring - M2 - Setup

---
+++ Spring/spring-exam/course1/module02/q4/src/main/java/com/spring/professional/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package com.spring.professional;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class ApplicationConfiguration {
+}
