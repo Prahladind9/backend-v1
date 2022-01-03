bf004aebe1ea14a7203326f8c0430aa534c2f027
Prahlad Rao <prahladskingdom@hotmail.com>
2022-01-03 21:25:52 +0530

    Sliding Window - 1

---
+++ DS/Algos/src/main/java/edu/prahlad/educativeio2/patternsForCodingQuestions/slidingWindow/MinSizeSubArraySum.java
@@ -0,0 +1,17 @@
+package edu.prahlad.educativeio2.patternsForCodingQuestions.slidingWindow;
+
+/**
+ * Given an array of positive numbers and a positive number ‘S,’
+ * find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’.
+ * Return 0 if no such subarray exists.
+ *
+ * Time:
+ * Space: 
+ */
+public class MinSizeSubArraySum {
+
+    public static int findMinSubArray(int S, int[] arr) {
+
+        return -1;
+    }
+}
