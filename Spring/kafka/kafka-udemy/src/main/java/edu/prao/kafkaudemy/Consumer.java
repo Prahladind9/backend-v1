f38932af8ddb951292145a0b38862e86104d4983
Prahlad Rao <prahladskingdom@hotmail.com>
2022-02-03 09:19:31 +0530

    init

--- Spring/kafka/kafka-udemy/src/main/java/edu/prao/kafkaudemy/Consumer.java
+++ Spring/kafka/kafka-udemy/src/main/java/edu/prao/kafkaudemy/Consumer.java
@@ -2,28 +2,38 @@ package edu.prao.kafkaudemy;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
 public class Consumer {
 
-    @Value(value = "${topic.name}")
-    private String topic;
+    @KafkaListener(topics = "${topic.name}", groupId = "${spring.kafka.group-id}")
+    public void iotListenTopic(@Payload List<ModifiableRandomEvent> iotData) throws JsonProcessingException {
+        logConsumedData(iotData);
+        log.info("message:{}",iotData);
+//        ObjectMapper mapper = new ObjectMapper();
+//        ModifiableRandomEvent event = mapper.readValue(message.value(), ModifiableRandomEvent.class);
+//        log.info("Message: "+message.topic());
+//        log.info("MessageKey: "+message.key());
+//        log.info("MessageValue: "+message.value());
+    }
 
-    @Value(value = "${spring.kafka.group-id}")
-    private String groupId;
-
-    @KafkaListener(topics = "${topic.name}", groupId = "${spring.kafka.group-id}", containerFactory = "iotKafkaListenerContainerFactory")
-    public void iotListenTopic(ConsumerRecord<Long, String> message) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        ModifiableRandomEvent event = mapper.readValue(message.value(), ModifiableRandomEvent.class);
-        log.info("Message: "+message.topic());
-        log.info("MessageKey: "+message.key());
-        log.info("MessageValue: "+message.value());
+    private void logConsumedData(List<ModifiableRandomEvent> iotData){
+        try {
+            ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+            String	consumedData = ow.writeValueAsString(iotData);
+            log.info(String.format("$$ -> Consumed message --> %s", consumedData));
+        } catch (JsonProcessingException e) {
+            log.error("Error while logging consumed data");
+        }
     }
 }
--- Spring/kafka/kafka-udemy/src/main/java/edu/prao/kafkaudemy/kafkaConsumerConfig.java
+++ Spring/kafka/kafka-udemy/src/main/java/edu/prao/kafkaudemy/kafkaConsumerConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.LongDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -14,30 +15,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafka
 public class kafkaConsumerConfig {
-
-    @Value(value = "${spring.kafka.bootstrap-servers}")
-    private String bootstrapAddress;
-
     @Value(value = "${spring.kafka.group-id}")
     private String groupId;
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean
-    public ConsumerFactory<Long, Object> iotConsumerFactory() {
+    public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        return new DefaultKafkaConsumerFactory<>(props, new LongDeserializer(), new JsonDeserializer<>(Object.class, false));
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        return props;
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<Long, Object> iotKafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<Long, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(iotConsumerFactory());
+    public ConsumerFactory<Long, ModifiableRandomEvent> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(
+                consumerConfigs(),
+                new LongDeserializer(),
+                new JsonDeserializer<>(ModifiableRandomEvent.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<Long, ModifiableRandomEvent> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<Long, ModifiableRandomEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
         return factory;
     }
 }
